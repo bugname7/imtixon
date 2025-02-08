165a89f8de4722a3cde3f5c25205ae31117ecd0d
bugname7 <valisher2106@gmail.com>
2025-02-08 20:05:56 +0500

    update components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,26 @@
 import React from 'react';
 import bgImage from '../assets/images/bgImage.png';
 import shake from '../assets/images/shape.svg';
+import dark from '../assets/images/dark.svg';
+import brother from '../assets/images/brother.png';
 
 function Header() {
     return (
         <div>
-            <header className="bg-slate-800 w-[85px] h-[800px] rounded-r-2xl rounded-t-2xl ">
+            <header className="bg-slate-800 w-[85px] h-[730px] rounded-r-2xl rounded-t-2xl flex flex-col justify-between items-center">
                 <div
-                    className="w-[85px] h-[85px] bg-cover bg-center flex items-center justify-center cursor-pointer w"
+                    className="w-[85px] h-[85px] bg-cover bg-center flex items-center justify-center cursor-pointer"
                     style={{ backgroundImage: `url(${bgImage})` }}
                 >
                     <img src={shake} alt="shake image" className="w-[40px] h-[40px]" />
+                </div>
+
+                <div className="w-full flex flex-col items-center">
+                    <div><img src={dark} alt="dark image" className="mt-4 cursor-pointer mb-4" />
+                    </div>
+                    <div className="w-full border-t border-slate-700 mt-4">
+                        <img src={brother} alt="brother image" className="mb-5 mt-8 w-[33px] mx-auto cursor-pointer" />
+                    </div>
                 </div>
             </header>
         </div>
