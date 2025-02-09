5a33ff0483cd5832bda23bd0c19ac968e9d5ef32
bugname7 <valisher2106@gmail.com>
2025-02-09 08:19:53 +0500

    header layout responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ import brother from '../assets/images/bro.png'
 function Header() {
     return (
         <div>
-            <header className='bg-slate-800 flex justify-between items-center' >
+            <header className='bg-slate-800 lg:justify-between flex justify-between items-center md:flex-col md:w-[70px] md:rounded-t-xl md:rounded-r-xl md:h-screen' >
                 <div className='w-[70px] h-[70px] bg-cover flex justify-center items-center'
                     style={{
 
@@ -18,11 +18,11 @@ function Header() {
                 >
                     <img src={shape} alt="shape image" className='w-[27px] h-[27px]' />
                 </div>
-                <div className='flex  items-center gap-5 '>
-                    <div className='p-4'>
+                <div className='flex  items-center gap-5 md:flex-col'>
+                    <div className='p-3'>
                         <img src={light} alt="light   image" className='cursor-pointer' />
                     </div>
-                    <div className='p-4 border-l border-slate-700'>
+                    <div className='p-4 border-l border-slate-700 md:border-t border-l-none'>
                         <img src={brother} alt="brohter image" className='cursor-pointer w-[35px] h-[35px]' />
                     </div>
                 </div>
