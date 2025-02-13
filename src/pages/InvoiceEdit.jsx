7a0d41f2fcfb46835ac76f784b0ac7db0684d57d
bugname7 <valisher2106@gmail.com>
2025-02-13 10:11:50 +0500

    updates

--- src/pages/InvoiceEdit.jsx
+++ src/pages/InvoiceEdit.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import InvoiceForm from '../components/invoice-form'
+import InvoiceForm from '../components/invoice-form/index.jsx'
 import { useParams } from 'react-router-dom';
 function InvoiceEdit() {
     const { id } = useParams();
